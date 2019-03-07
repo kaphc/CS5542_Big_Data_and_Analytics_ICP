f2a72d4b050edfbd442edfd3f64b3a7ab3050096
kaphc <kaphc@mail.umkc.edu>
2019-03-07 15:58:03 -0600

    ICP-6
    icp-6

---
+++ ICP-5/video.py
@@ -0,0 +1,9 @@
+import cv2
+vidcap = cv2.VideoCapture('SampleVideo_1280x720_2mb.mp4')
+success,image = vidcap.read()
+count = 0
+while success:
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
