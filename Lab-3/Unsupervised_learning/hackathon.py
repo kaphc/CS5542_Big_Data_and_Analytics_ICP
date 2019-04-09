8c85c83d29880eb2c971cfc4e2e0338adb98e122
kaphc <kaphc@mail.umkc.edu>
2019-04-09 17:44:55 -0500

    ICP-9

---
+++ Lab-3/Unsupervised_learning/hackathon.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+
+im = Image.open("4   .jpg")
+text = pytesseract.image_to_string(im, lang="eng")
+print(text)
