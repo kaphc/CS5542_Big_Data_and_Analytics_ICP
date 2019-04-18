2116c44511b1a7ae647a775f18e27f1d001fa8ac
kaphc <kaphc@mail.umkc.edu>
2019-04-18 15:47:38 -0500

    icp-10

---
+++ ICP-10/Image_Classification/src/main/scala/IPSettings.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by pradyumnad on 19/07/15.
+  */
+object IPSettings {
+
+  val PATH = "data3/"
+  val INPUT_DIR = PATH + "train"
+  val TEST_INPUT_DIR = PATH + "test2"
+  val FEATURES_PATH = PATH + "model/features"
+
+  val KMEANS_PATH = PATH + "model/clusters"
+  val HISTOGRAM_PATH = PATH + "model/histograms"
+  val RANDOM_FOREST_PATH = PATH + "model/nbmodel"
+}
